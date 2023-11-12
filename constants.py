9cf8f6678da9e71f980023a39f814b2c54fa15e2
Unitato <leshkaferg@gmail.com>
2023-11-11 18:21:29 -0800

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,12 @@
+# constants.py
+
+# Base URLs for KnowBe4 API server locations
+BASE_URLS = {
+    "US": "https://us.api.knowbe4.com",
+    "EU": "https://eu.api.knowbe4.com",
+    "CA": "https://ca.api.knowbe4.com",
+    "UK": "https://uk.api.knowbe4.com",
+    "DE": "https://de.api.knowbe4.com",
+}
+
+# Add other constants as needed
