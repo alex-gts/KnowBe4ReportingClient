4fbc0ca4d8540268a52a3e59b91c49323b23d9b3
Alex Garrison <alex.garrison@discoverieplc.com>
2024-12-09 21:54:06 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from setuptools import setup, find_packages
 
+
+def parse_requirements(filename):
+    """Load requirements from a pip requirements file."""
+    with open(filename) as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
+
+
+requirements = parse_requirements("requirements.txt")
+
 setup(
     name="KnowBe4Client",
     version="0.1.0",
@@ -8,6 +17,7 @@ setup(
     description="Python interface to interact with the KnowBe4 API",
     long_description_content_type="text/markdown",
     packages=find_packages(),
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
